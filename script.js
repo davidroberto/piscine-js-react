26969abe4585e674c6c6df1c43da8ccb79c7f9ad
David Robert <contact@david-robert.fr>
2024-03-28 14:58:45 +0100

    low cost react

--- script.js
+++ script.js
@@ -1,12 +1,43 @@
+const blogArticles = [
+  {
+    title: "The Top 10 ES6 Features Every Web Developer Must Know",
+    url: "https://webapplog.com/es6",
+    image: "https://upload.wikimedia.org/wikipedia/commons/6/6a/JavaScript-logo.png",
+  },
+  {
+    title: "The Top 11 ES6 Features Every Web Developer Must Know",
+    url: "https://webapplog.com/es6",
+    image: "https://upload.wikimedia.org/wikipedia/commons/6/6a/JavaScript-logo.png",
+  },
+  {
+    title: "The Top 12 ES6 Features Every Web Developer Must Know",
+    url: "https://webapplog.com/es6",
+    image: "https://upload.wikimedia.org/wikipedia/commons/6/6a/JavaScript-logo.png",
+  },
+];
+
 const bodyElement = document.querySelector("body");
 
-const renderElement = (tagName, text) => {
+const renderElement = (tagName, text, attributs) => {
   const element = document.createElement(tagName);
 
   element.textContent = text;
 
+  for (const key in attributs) {
+    if (attributs.hasOwnProperty(key)) {
+      element.setAttribute(key, attributs[key]);
+    }
+  }
+
   bodyElement.appendChild(element);
 };
 
-renderElement("div", "hello");
-renderElement("h1", "Je suis un h1");
+blogArticles.map((article) => {
+  renderElement("p", article.title, {});
+  renderElement("img", "", {
+    src: article.image,
+  });
+  renderElement("a", "Cliquez ici", {
+    href: article.url,
+  });
+});
